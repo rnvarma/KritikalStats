597471d44bd7d456217ee6dd9352d97b8ef73497
Rohan Varma <rnvarma@andrew.cmu.edu>
2014-07-23 10:59:15 -0700

    more sidebar changes

--- debate/static/js/main_page.js
+++ debate/static/js/main_page.js
@@ -132,7 +132,8 @@ function populateMain (tournament, data){
 
 $(document).ready(function() {
   var tournament = $(".main_page").attr("data-tournament");
-  console.log(tournament)
+  console.log($("#active-" + tournament)[0]);
+  $("#active-" + tournament).addClass("active");
 
   var divMain = document.getElementById('container-' + tournament + '-Main')
   var h1 = document.createElement("h1");
